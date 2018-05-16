3190f4c0cd4dcb82f90199951dcddfaaa285705a
Sasha Kipnis <akipnis@purdue.edu>
2018-05-16 17:12:21 -0400

    Add delete button and (not functional) buttons for filtering

--- puppypicker.js
+++ puppypicker.js
@@ -1,4 +1,7 @@
-const form = document.querySelector('form#userForm')
+const form = document.querySelector('form#addPuppy')
+const desireLevel = ['Want', 'Really Want', 'Need', 'Cannot Live Without']
+
+let puppyArray = new Array()
 
 const renderListItem = function(label, value){
     const item = document.createElement('li')
@@ -18,10 +21,21 @@ const renderListItem = function(label, value){
     return item
 }
 
+const handleDelete = function(ev){
+    ev.preventDefault()
+    const listItem = ev.target
+    listItem.parentNode.remove()
+    puppyArray.splice(puppyArray.indexOf(listItem), 1)
+}
+
 const renderList = function(data){
     const list = document.createElement('ul')
+    const button = document.createElement('button')
+    button.addEventListener('click', handleDelete)
+    button.textContent = 'Delete'
     Object.keys(data).map(function(label) {
         list.appendChild(renderListItem(label, data[label]))
+        list.appendChild(button)
     })
     return list
 }
@@ -34,14 +48,29 @@ const handleSubmit = function(ev){
 
     const puppy = {
         'Puppy: ': form.puppyType.value,
-        'Desire Level: ': form.desireLevel.value
+        'Desire: ': form.desireLevel.value
     }
+    
 
     puppies.appendChild(renderList(puppy))
+    puppyArray.push(renderList(puppy))
 
     form.reset()
     form.puppyType.focus()
-
 }
 
 form.addEventListener('submit', handleSubmit)
+
+const handleDisplayChoice = function(){
+    const current = document.getElementsByClassName("active")
+    current[0].className = current[0].className.replace(" active", "")
+    this.className += " active";
+}
+
+//change which button is active
+const buttonContainer = document.getElementById('myBtnContainer')
+const buttons = buttonContainer.getElementsByClassName('btn')
+for(var i = 0; i < buttons.length; i++){
+    buttons[i].addEventListener("click", handleDisplayChoice)  
+}
+
